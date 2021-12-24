396eefa6f8b0a71d0e43fc116dd493b902fd54ac
Rob174 <robin.128.456@gmail.com>
2021-12-24 17:22:03 -0400

    Initial code

---
+++ commitchecker/src/CommitChecker.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import subprocess
+from typing import List, Optional
+import tensorflow as tf
+from time import strftime
+import os
+
+class CommitChecker:
+    """
+    Class to check if there are any uncommitted changes and return last commit message
+    The main script must be run from the root of the project
+    """
+    def __init__(self, target_extensions:Optional[List[str]]=None):
+        if target_extensions is None:
+            self.target_extensions = [".py"]
+        else:
+            self.target_extensions = target_extensions
+        commit,commit_message = self.check()
+        runs_dir = Path(".").joinpath("runs")
+        self.log_dir = runs_dir.joinpath(f"{strftime('%Y%m%d-%Hh%Mmin%Ss')}_{commit}")
+        if not os.path.exists(str(runs_dir)):
+            print(f"WARNING: runs directory does not exist. Creating it at {runs_dir.resolve()}")
+            os.mkdir(str(runs_dir))
+        # Add commit hash to the tr,valid logs
+        for folder in ["train", "validation"]:
+            file_writer = tf.summary.create_file_writer(str(self.log_dir.joinpath(folder)))
+            with file_writer.as_default():
+                tf.summary.text("Description commit", commit_message, step=0)
+                file_writer.flush()
+    def check(self):
+        """Check if there are any uncommitted changes and return last commit and associated commmit message"""
+        commit = subprocess.check_output(
+            ['git', 'rev-parse', '--short', 'HEAD']).decode("utf-8").strip()
+        commit_message = subprocess.check_output(
+            ['git', 'log', '--oneline']).decode("utf-8").strip().split("\n")[0]
+        changes = subprocess.check_output(
+            ['git', 'diff', '--name-only', "--", "*.py"]).decode("ascii").split("\n")[:-1]
+        if len(changes):
+            changes_str = "\n".join(list(map(lambda x: '\t- ' + x, changes)))
+            input(
+                f"There are {len(changes)} uncommitted python files:\n{changes_str}\n Are you sure you want to continue ?")
+        return commit,commit_message
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'commitchecker',
+  packages = ['commitchecker'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Check that all code is correctly committed',
+  author = 'Robin M',
+  author_email = 'robin.128.456@gmail.com',
+  url = 'https://github.com/Rob174/CommitChecker',
+  download_url = 'https://github.com/Rob174/CommitChecker/0.1',
+  keywords = ['commit', 'ai' "git"], 
+  classifiers = [],
+)
